d61c8343a01fc520638695b4377a6cc6dd07d137
sugasaki <sugasaki@gmail.com>
2021-08-29 13:52:11 +0900

    add project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @param args Array<String>
+ */
+fun main(args: Array<String>) {
+    println("Hello Knapsack Problem!")
+
+    val weights = listOf(3, 4, 1, 2, 3)
+    val values = listOf(2, 3, 2, 3, 6)
+    val weightMax = 10
+}
